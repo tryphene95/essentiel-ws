fc49ef1e005b5dcffccdc4d9a22a47b971ec7e66
tryphène95 <tryphenediomande99@gmail.com>
2023-03-31 21:43:58 +0000

    exercice

---
+++ js/script.js
@@ -0,0 +1,31 @@
+exercice1
+
+
+let value = 2
+if(typeof value ==='number') {console.log("la value est un number");
+}else if(typeof value === 'string') {
+    console.log("la value est une chaine de caractere");
+}
+
+
+switch (value) {
+    case '0':
+    case 0:
+        console.log("la valeur est egale a 0");
+        break;
+    case'1':
+    case 1:
+            console.log("la valeur est superieur a 0");
+        break;
+        case'2':
+        case 2:
+            console.log("la valeur est superieura 0");
+        break;
+        default:
+            console.log("value n'est pas valide")
+
+
+}
+
+
+Exercice
